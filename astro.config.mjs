40e1b60b1d92bbe19c2dabbffc85025f7ceb704e
what-it-izz <main4izzy@gmail.com>
2025-03-27 19:38:07 -0700

    deploy for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://what-it-izz.github.io',
-    base: '/atro-tutorial/'
+    base: '/atro-tutorial'
 });
